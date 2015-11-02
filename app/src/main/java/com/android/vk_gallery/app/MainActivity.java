9906e582cb0a30a28810d4c893f04d5678d6c372
D-Moskalyov <moscalevd@Gmail.com>
2015-11-03 01:03:59 +0300

    VK_API; Retrofit; Realm; Fresco; Gson added

--- app/src/main/java/com/android/vk_gallery/app/MainActivity.java
+++ app/src/main/java/com/android/vk_gallery/app/MainActivity.java
@@ -1,13 +1,49 @@
 package com.android.vk_gallery.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.util.VKUtil;
 
 
 public class MainActivity extends FragmentActivity {
 
+    //protected MyApplication app;
+    private static String[] sMyScope = new String[]{VKScope.PHOTOS};
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        //VKSdk.initialize(getApplicationContext());
+        //app = (MyApplication)getApplication();
+        //app.customAppMethod();
+
+        VKSdk.login(this, sMyScope);
+        //VKSdk.login(Fragment runningFragment, String... scope);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // ѕользователь успешно авторизовалс€
+                VKRequest request = VKApi.users().get();
+            }
+
+            @Override
+            public void onError(VKError error) {
+                // ѕроизошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
---
+++ app/src/main/java/com/android/vk_gallery/app/MyApplication.java
@@ -0,0 +1,31 @@
+package com.android.vk_gallery.app;
+
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKAccessTokenTracker;
+import com.vk.sdk.VKSdk;
+
+public class MyApplication extends android.app.Application {
+
+    VKAccessTokenTracker vkAccessTokenTracker = new VKAccessTokenTracker() {
+        @Override
+        public void onVKAccessTokenChanged(@Nullable VKAccessToken oldToken, @Nullable VKAccessToken newToken) {
+            if (newToken == null) {
+                Toast.makeText(MyApplication.this, "AccessToken invalidated", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MyApplication.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        vkAccessTokenTracker.startTracking();
+        VKSdk.initialize(this);
+    }
+}
