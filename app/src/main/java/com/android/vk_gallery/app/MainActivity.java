6bee1bafb880d4e42cd369c1c71ac8ff405ea0e6
D-Moskalyov <moscalevd@Gmail.com>
2015-11-02 21:53:13 +0300

    First init

---
+++ app/src/main/java/com/android/vk_gallery/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.vk_gallery.app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class MainActivity extends FragmentActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
